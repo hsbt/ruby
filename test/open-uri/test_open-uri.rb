39fd899928f737628b3e670da8792e8e69dd6e9a
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-06-27 15:42:33 +0900

    Extract SimpleHTTPServer

--- test/open-uri/test_open-uri.rb
+++ test/open-uri/test_open-uri.rb
@@ -1,123 +1,12 @@
 # frozen_string_literal: true
 require 'test/unit'
 require 'open-uri'
-require 'socket'
-require 'webrick'
-require 'webrick/httpproxy'
 begin
   require 'zlib'
 rescue LoadError
 end
+require_relative 'simple_http_server'
 
-class SimpleHTTPServer
-  def initialize(port)
-    @server = TCPServer.new('127.0.0.1', port)
-    @routes = {}
-  end
-
-  def mount_proc(path, proc = nil, &block)
-    if proc
-      @routes[path] = proc
-    else
-      @routes[path] = block
-    end
-  end
-
-  def start
-    @thread = Thread.new do
-      loop do
-        client = @server.accept
-        request_line = client.gets
-        headers = {}
-        while (line = client.gets) && (line != "\r\n")
-          key, value = line.chomp.split(/:\s*/, 2)
-          headers[key] = value
-        end
-        next unless request_line
-
-        method, path, _ = request_line.split(' ')
-        handle_request(client, method, path, headers)
-      end
-    end
-  end
-
-  def shutdown
-    @thread.kill
-    @server.close
-  end
-
-  private
-
-  def handle_request(client, method, path, headers)
-    if @routes.key?(path)
-      proc = @routes[path]
-      req = Struct.new(:request_method, :path_info, :headers).new(method, path, headers)
-      res = Struct.new(:body, :status).new("", 200)
-      proc.call(req, res)
-      client.print "HTTP/1.1 #{res.status}\r\nContent-Type: text/plain\r\n\r\n#{res.body}"
-    else
-      client.print "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found"
-    end
-  ensure
-    client.close
-  end
-end
-
-class SimpleHTTPProxyServer
-  def initialize(port, auth_proc = nil)
-    @server = TCPServer.new('127.0.0.1', port)
-    @auth_proc = auth_proc
-  end
-
-  def start
-    @thread = Thread.new do
-      loop do
-        client = @server.accept
-        request_line = client.gets
-        headers = {}
-        while (line = client.gets) && (line != "\r\n")
-          key, value = line.chomp.split(/:\s*/, 2)
-          headers[key] = value
-        end
-        next unless request_line
-
-        method, path, _ = request_line.split(' ')
-        handle_request(client, method, path, request_line, headers)
-      end
-    end
-  end
-
-  def shutdown
-    @thread.kill
-    @server.close
-  end
-
-  private
-
-  def handle_request(client, method, path, request_line, headers)
-    if @auth_proc
-      req = Struct.new(:request_method, :path_info, :request_line, :headers).new(method, path, request_line, headers)
-      res = Struct.new(:body, :status).new("", 200)
-      @auth_proc.call(req, res)
-      if res.status != 200
-        client.print "HTTP/1.1 #{res.status}\r\nContent-Type: text/plain\r\n\r\n#{res.body}"
-        return
-      end
-    end
-
-    uri = URI(path)
-    proxy_request(uri, client)
-  ensure
-    client.close
-  end
-
-  def proxy_request(uri, client)
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      response = http.get(uri.path)
-      client.print "HTTP/1.1 #{response.code}\r\nContent-Type: #{response.content_type}\r\n\r\n#{response.body}"
-    end
-  end
-end
 class TestOpenURI < Test::Unit::TestCase
 
   def with_http(log_tester=lambda {|log| assert_equal([], log) })
